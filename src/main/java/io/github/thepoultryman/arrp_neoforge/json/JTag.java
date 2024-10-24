47b47f2ff0d551998a862598555fc5e8cdda1044
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-10-24 14:37:05 -0400

    feat: implement JTag

--- src/main/java/io/github/thepoultryman/arrp_neoforge/json/JTag.java
+++ src/main/java/io/github/thepoultryman/arrp_neoforge/json/JTag.java
@@ -1,4 +1,27 @@
 package io.github.thepoultryman.arrp_neoforge.json;
 
-public class JTag {
+import io.github.thepoultryman.arrp_neoforge.util.BaseCloneable;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JTag extends BaseCloneable<JTag> {
+    private Boolean replace = false;
+    private final List<String> values = new ArrayList<>();
+
+    public JTag replace() {
+        this.replace = true;
+        return this;
+    }
+
+    public JTag add(ResourceLocation item) {
+        this.values.add(item.toString());
+        return this;
+    }
+
+    public JTag addTag(ResourceLocation tag) {
+        this.values.add("#" + tag.getNamespace() + ":" + tag.getPath());
+        return this;
+    }
 }
