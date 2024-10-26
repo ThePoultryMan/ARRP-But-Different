4360af51107f3b7a41e783d28f14364bf2b03b65
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-10-25 21:01:50 -0400

    feat: test mod

---
+++ test/src/main/java/io/github/thepoultryman/arrp_test/ARRPTest.java
@@ -0,0 +1,20 @@
+package io.github.thepoultryman.arrp_test;
+
+import com.mojang.logging.LogUtils;
+import io.github.thepoultryman.arrp_neoforge.ARRPForNeoForge;
+import io.github.thepoultryman.arrp_neoforge.api.event.ARRPEvent;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(ARRPTest.MOD_ID)
+public class ARRPTest {
+    public static final String MOD_ID = "arrp_test";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ARRPTest() {
+        ARRPForNeoForge.ARRP_EVENT_BUS.addListener((ARRPEvent.BeforeUserEvent event) -> {
+            LOGGER.info("Running BeforeUser event test");
+        });
+    }
+}
