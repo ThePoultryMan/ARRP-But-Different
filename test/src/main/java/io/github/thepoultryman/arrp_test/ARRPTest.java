be0b67b405f8826ab44d69347ffeae5c1a0e236c
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-10-26 14:21:21 -0400

    test: language

--- test/src/main/java/io/github/thepoultryman/arrp_test/ARRPTest.java
+++ test/src/main/java/io/github/thepoultryman/arrp_test/ARRPTest.java
@@ -4,11 +4,11 @@ import com.mojang.logging.LogUtils;
 import io.github.thepoultryman.arrp_neoforge.ARRPForNeoForge;
 import io.github.thepoultryman.arrp_neoforge.api.RuntimeResourcePack;
 import io.github.thepoultryman.arrp_neoforge.api.event.ARRPEvent;
+import io.github.thepoultryman.arrp_neoforge.json.JLang;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(ARRPTest.MOD_ID)
 public class ARRPTest {
     public static final String MOD_ID = "arrp_test";
@@ -18,6 +18,9 @@ public class ARRPTest {
         ARRPForNeoForge.ARRP_EVENT_BUS.addListener((ARRPEvent.BeforeUserEvent event) -> {
             LOGGER.info("Running BeforeUser event test");
             RuntimeResourcePack pack = RuntimeResourcePack.create(ResourceLocation.fromNamespaceAndPath(MOD_ID, "before_user"));
+            pack.addLang(ResourceLocation.fromNamespaceAndPath(MOD_ID, "en_us"),
+                    new JLang().addBlockTranslation(ResourceLocation.withDefaultNamespace("torch"), "Torch but it's different but it's not so it's the same")
+            );
             event.addPack(pack);
         });
     }
