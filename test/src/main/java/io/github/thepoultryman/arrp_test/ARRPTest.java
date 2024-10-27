40e0f5e1b6c4133625db3bd0cc750464b0f2d1c1
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-10-27 16:13:43 -0400

    test: add block model test

--- test/src/main/java/io/github/thepoultryman/arrp_test/ARRPTest.java
+++ test/src/main/java/io/github/thepoultryman/arrp_test/ARRPTest.java
@@ -5,6 +5,7 @@ import io.github.thepoultryman.arrp_neoforge.ARRPForNeoForge;
 import io.github.thepoultryman.arrp_neoforge.api.RuntimeResourcePack;
 import io.github.thepoultryman.arrp_neoforge.api.event.ARRPEvent;
 import io.github.thepoultryman.arrp_neoforge.json.JLang;
+import io.github.thepoultryman.arrp_neoforge.json.model.*;
 import io.github.thepoultryman.arrp_neoforge.json.state.JBlockModel;
 import io.github.thepoultryman.arrp_neoforge.json.state.JState;
 import io.github.thepoultryman.arrp_neoforge.json.state.JVariant;
@@ -46,6 +47,87 @@ public class ARRPTest {
                                     new JVariant().put("facing", Direction.NORTH,
                                             new JBlockModel(ResourceLocation.withDefaultNamespace("block/wall_torch")).y(270)
                                     )
+                            )
+            );
+            pack.addModel(ResourceLocation.withDefaultNamespace("block/torch"),
+                    new JModel()
+                            .noAmbientOcclusion()
+                            .textures(
+                                    new JTextures().add("torch", "minecraft:block/torch")
+                                            .particle("#torch")
+                            )
+                            .element(
+                                    new JElement()
+                                            .from(7, 0, 7)
+                                            .to(9, 10, 9)
+                                            .noShade()
+                                            .faces(
+                                                    new JFaces()
+                                                            .down(
+                                                                    new JFace("torch")
+                                                                            .uv(7, 13, 9, 15)
+                                                            )
+                                                            .up(
+                                                                    new JFace("torch")
+                                                                            .uv(7, 6, 9, 8)
+                                                            )
+                                            ),
+                                    new JElement()
+                                            .from(7, 0, 0)
+                                            .to(9, 16, 16)
+                                            .noShade()
+                                            .faces(
+                                                    new JFaces()
+                                                            .west(
+                                                                    new JFace("torch")
+                                                                            .uv(0, 0, 16, 16)
+                                                            )
+                                                            .east(
+                                                                    new JFace("torch")
+                                                                            .uv(0, 0, 16, 16)
+                                                            )
+                                            ),
+                                    new JElement()
+                                            .from(0, 0, 7)
+                                            .to(16, 16, 9)
+                                            .noShade()
+                                            .faces(
+                                                    new JFaces()
+                                                            .north(
+                                                                    new JFace("torch")
+                                                                            .uv(0, 0, 16, 16)
+                                                            )
+                                                            .south(
+                                                                    new JFace("torch")
+                                                                            .uv(0, 0, 16, 16)
+                                                            )
+                                            ),
+                                    new JElement()
+                                            .from(6, 0, 6)
+                                            .to(10, 2, 10)
+                                            .faces(
+                                                    new JFaces()
+                                                            .north(
+                                                                    new JFace("missing")
+                                                                            .uv(0, 0, 4, 2)
+                                                            )
+                                                            .east(
+                                                                    new JFace("missing")
+                                                                            .uv(0, 0, 4, 2)
+                                                            )
+                                                            .south(
+                                                                    new JFace("missing")
+                                                                            .uv(0, 0, 4, 2)
+                                                            )
+                                                            .west(
+                                                                    new JFace("missing")
+                                                                            .uv(0, 0, 4, 2)
+                                                            )
+                                                            .up(
+                                                                    new JFace("missing")
+                                                                            .uv(0, 0, 4, 4)
+                                                            )
+                                            )
                             )
             );
             event.addPack(pack);
