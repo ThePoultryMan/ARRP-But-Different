835d0bbb03040499b12be153326ebda4000bb311
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-10-26 22:07:22 -0400

    docs: JTag

--- src/main/java/io/github/thepoultryman/arrp_neoforge/json/JTag.java
+++ src/main/java/io/github/thepoultryman/arrp_neoforge/json/JTag.java
@@ -10,16 +10,30 @@ public class JTag extends BaseCloneable<JTag> {
     private Boolean replace = false;
     private final List<String> values = new ArrayList<>();
 
+    /**
+     * Sets "replace: true" in the generated tag
+     * @return The current {@link JTag} instance
+     */
     public JTag replace() {
         this.replace = true;
         return this;
     }
 
-    public JTag add(ResourceLocation item) {
-        this.values.add(item.toString());
+    /**
+     * Adds an entry into the tag.
+     * @param entry The entry added to the tag.
+     * @return The current {@link JTag} instance
+     */
+    public JTag add(ResourceLocation entry) {
+        this.values.add(entry.toString());
         return this;
     }
 
+    /**
+     * Adds an entry into the tag. The added entry is a tag.
+     * @param tag The tag added to the tag
+     * @return The current {@link JTag} instance
+     */
     public JTag addTag(ResourceLocation tag) {
         this.values.add("#" + tag.getNamespace() + ":" + tag.getPath());
         return this;
