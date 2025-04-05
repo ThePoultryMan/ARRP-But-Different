1bbc9265f06a36be1633fb3b6b9da7688ca6e0a7
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-04-04 22:29:53 -0400

    feat: bucket entity data component

---
+++ common/src/main/java/io/github/thepoultryman/arrp_but_different/json/recipe/component/JBucketEntityDataComponent.java
@@ -0,0 +1,26 @@
+package io.github.thepoultryman.arrp_but_different.json.recipe.component;
+
+import com.google.gson.annotations.SerializedName;
+import net.minecraft.resources.ResourceLocation;
+
+public class JBucketEntityDataComponent extends AbstractJComponent {
+    private final ResourceLocation id;
+    @SerializedName("HuntingCooldown")
+    private int huntingCooldown = 0;
+    @SerializedName("BucketVariantTag")
+    private int bucketVariantTag = 0;
+
+    public JBucketEntityDataComponent(ResourceLocation id) {
+        this.id = id;
+    }
+
+    public JBucketEntityDataComponent huntingCooldown(int huntingCooldown) {
+        this.huntingCooldown = huntingCooldown;
+        return this;
+    }
+
+    public JBucketEntityDataComponent bucketVariantTag(int bucketVariantTag) {
+        this.bucketVariantTag = bucketVariantTag;
+        return this;
+    }
+}
