e009d58cf416daafdece81290a84696dd8b6b78a
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-04-06 18:24:10 -0400

    feat: profile, provides banner pattern, and provides trim material components

--- common/src/main/java/io/github/thepoultryman/arrp_but_different/json/recipe/JResult.java
+++ common/src/main/java/io/github/thepoultryman/arrp_but_different/json/recipe/JResult.java
@@ -11,9 +11,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.LockCode;
 import net.minecraft.world.item.Instrument;
 import net.minecraft.world.item.alchemy.PotionContents;
-import net.minecraft.world.item.component.LodestoneTracker;
-import net.minecraft.world.item.component.MapDecorations;
-import net.minecraft.world.item.component.MapPostProcessing;
+import net.minecraft.world.item.component.*;
 import net.minecraft.world.level.block.entity.PotDecorations;
 
 import java.util.ArrayList;
@@ -259,6 +257,18 @@ public class JResult extends BaseCloneable<JResult> {
 
     public JResult potionDurationScale(float durationScale) {
         return this.component("minecraft:potion_duration_scale", new JSimpleComponent<>(durationScale));
+    }
+
+    public JResult profile(ResolvableProfile profile) {
+        return this.component("minecraft:profile", new JCodecComponent<>(profile, ResolvableProfile.CODEC));
+    }
+
+    public JResult providesBannerPatterns(ResourceLocation tag) {
+        return this.component("minecraft:provides_banner_pattern", new JSimpleComponent<>("#" + tag.toString()));
+    }
+
+    public JResult providesTrimMaterial(ProvidesTrimMaterial material) {
+        return this.component("minecraft:provides_trim_material", new JCodecComponent<>(material, ProvidesTrimMaterial.CODEC));
     }
 
     public JResult catVariant(ResourceLocation resourceLocation) {
