e5fcb2b928571d8bf6bcf8baf360cbd7e54673f3
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-04-29 21:22:10 -0400

    fix: forge config api port to 1.21.5

--- fabric/src/main/java/io/github/thepoultryman/arrp_but_different/fabric/ARRPButFabric.java
+++ fabric/src/main/java/io/github/thepoultryman/arrp_but_different/fabric/ARRPButFabric.java
@@ -1,6 +1,6 @@
 package io.github.thepoultryman.arrp_but_different.fabric;
 
-import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.api.v5.ConfigRegistry;
 import io.github.thepoultryman.arrp_but_different.ARRPCommon;
 import io.github.thepoultryman.arrp_but_different.ARRPConfigSpec;
 import net.fabricmc.api.ModInitializer;
@@ -9,6 +9,6 @@ import net.neoforged.fml.config.ModConfig;
 public class ARRPButFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-        NeoForgeConfigRegistry.INSTANCE.register(ARRPCommon.MOD_ID, ModConfig.Type.COMMON, ARRPConfigSpec.CONFIG);
+        ConfigRegistry.INSTANCE.register(ARRPCommon.MOD_ID, ModConfig.Type.COMMON, ARRPConfigSpec.CONFIG);
     }
 }
