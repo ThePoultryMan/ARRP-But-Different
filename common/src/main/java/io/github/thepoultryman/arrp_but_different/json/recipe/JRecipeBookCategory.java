5b22216514c0799f992590be2783a96e6d656038
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-05-04 14:04:50 -0400

    feat: crafting_transmute recipe

---
+++ common/src/main/java/io/github/thepoultryman/arrp_but_different/json/recipe/JRecipeBookCategory.java
@@ -0,0 +1,8 @@
+package io.github.thepoultryman.arrp_but_different.json.recipe;
+
+public class JRecipeBookCategory {
+    public static final String EQUIPMENT = "equipment";
+    public static final String BUILDING = "building";
+    public static final String MISCELLANEOUS = "misc";
+    public static final String REDSTONE = "redstone";
+}
